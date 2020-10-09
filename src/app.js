ace24406cc16bb4cf1d33045448e4667cd237fa9
pullso <p.dubitskiy@ya.ru>
2020-10-10 01:15:21 +0500

    add webpack.config.js

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import './static/style.css'
+
+console.log('app working')
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
   entry: './src/app.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname,'./public'),
+  },
+  devServer: {
+    port: 4242
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
+
+
